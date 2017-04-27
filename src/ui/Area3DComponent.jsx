02aad134931dbbfc7c75022199d35a4b8658ac9d
Daan Vanden Bosch <daan@daanvandenbosch.com>
2017-04-27 17:46:24 +0200

    The 3D view of the currently selected area now resizes with the browser window.

--- src/ui/Area3DComponent.jsx
+++ src/ui/Area3DComponent.jsx
@@ -6,15 +6,31 @@ import { Renderer } from '../rendering/Renderer';
 import { Area, Quest, VisibleQuestEntity } from '../domain';
 
 export class Area3DComponent extends React.Component {
-    _renderer = new Renderer({
-        on_select: this._on_select.bind(this)
-    });
+    _renderer: Renderer;
 
-    render() {
-        return <div style={this.props.style} ref={this._modify_dom} />;
+    constructor(...args) {
+        super(...args);
+
+        // _renderer has to be assigned here so that it happens after _on_select is assigned.
+        this._renderer = new Renderer({
+            on_select: this._on_select
+        });
     }
 
-    componentWillReceiveProps({quest, area, model}: { quest: ?Quest, area: ?Area, model: ?Object3D }) {
+    render() {
+        const wrapper_style = { overflow: 'hidden', ...this.props.style };
+        return <div style={wrapper_style} ref={this._modify_dom} />;
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._on_resize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._on_resize);
+    }
+
+    componentWillReceiveProps({ quest, area, model }: { quest: ?Quest, area: ?Area, model: ?Object3D }) {
         if (model) {
             this._renderer.set_model(model);
         } else {
@@ -31,7 +47,12 @@ export class Area3DComponent extends React.Component {
         div.appendChild(this._renderer.dom_element);
     }
 
-    _on_select(entity: ?VisibleQuestEntity) {
+    _on_select = (entity: ?VisibleQuestEntity) => {
         entity_selected(entity);
+    }
+
+    _on_resize = () => {
+        const wrapper_div = this._renderer.dom_element.parentNode;
+        this._renderer.set_size(wrapper_div.clientWidth, wrapper_div.clientHeight);
     }
 }
